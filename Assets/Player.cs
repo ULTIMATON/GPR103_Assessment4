f14715bfd3fb0d266cd12ae1d57670c5aaab1ea6
Clinton <Clintonhui33@gmail.com>
2020-08-17 19:35:07 +1000

    16/08

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -16,6 +16,13 @@ public class Player : MonoBehaviour
     public bool playerIsAlive = true; //Is the player currently alive?
     public bool playerCanMove = false; //Can the player currently move?
 
+    public AudioSource myAudioSource;
+    public AudioClip leapVerticalSound;
+    public AudioClip leapHorizontalSound;
+    public AudioClip deathSound;
+
+    public GameObject dyingEffectPrefab;
+
     public GameManager myGameManager; //A reference to the GameManager in the scene.
 
     // Start is called before the first frame update
@@ -32,18 +39,26 @@ public class Player : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.W) && transform.position.y < myGameManager.levelConstraintTop)
             {
                 transform.position = transform.position + new Vector3(0, 1, 0);
+                myAudioSource.clip = leapVerticalSound;
+                myAudioSource.Play();
             }
             else if (Input.GetKeyDown(KeyCode.S) && transform.position.y > myGameManager.levelConstraintBottom)
             {
                 transform.position = transform.position + new Vector3(0, -1, 0);
+                myAudioSource.clip = leapVerticalSound;
+                myAudioSource.Play();
             }
             else if (Input.GetKeyDown(KeyCode.A) && transform.position.x > myGameManager.levelConstraintLeft)
             {
                 transform.position = transform.position + new Vector3(-1, 0, 0);
+                myAudioSource.clip = leapHorizontalSound;
+                myAudioSource.Play();
             }
             else if (Input.GetKeyDown(KeyCode.D) && transform.position.x < myGameManager.levelConstraintRight)
             {
                 transform.position = transform.position + new Vector3(1, 0, 0);
+                myAudioSource.clip = leapHorizontalSound;
+                myAudioSource.Play();
             }
 
         }
@@ -51,16 +66,22 @@ public class Player : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "Vehicle")
+        if(playerIsAlive == true)
         {
-            print("hit");
-            playerCanMove = false;
-            playerIsAlive = false;
+            if (collision.transform.tag == "Vehicle")
+            {
+                print("hit");
+                playerCanMove = false;
+                playerIsAlive = false;
+                myAudioSource.clip = deathSound;
+                myAudioSource.Play();
+            }
+            else
+            {
+                print(collision.transform.tag);
+            }
         }
-        else
-        {
-            print(collision.transform.tag);
-        }
+
     }
 
 }
